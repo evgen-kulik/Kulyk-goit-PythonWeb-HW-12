b6be3f4e426b56d2dbae4b7bbeb6c2f57d908117
evgen-kulik <e.kulik.atom@gmail.com>
2023-09-10 18:45:41 +0200

    Додано CORS
    Додано "Cross-Origin Resource Sharing".  Помилка  "pydantic.error_wrappers.ValidationError: 10 validation errors for UserResponse" при виконанні "SignUp".

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from src.routes import contacts, users, auth
 import redis.asyncio as redis
 from src.conf.config import settings  # для обмеження кількості запитів
@@ -17,6 +18,15 @@ app.include_router(users.router, prefix="/api")
 async def startup():
     r = await redis.Redis(host=settings.redis_host, port=settings.redis_port, db=0)
     await FastAPILimiter.init(r)
+
+# Додаємо CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # список джерел, яким дозволено доступ до застосунку
+    allow_credentials=True,  # True означає, що дозволені кросдоменні запити з урахуванням облікових даних
+    allow_methods=["*"],  # список дозволених методів HTTP
+    allow_headers=["*"],  # список дозволених заголовків HTTP
+)
 
 
 @app.get("/")
